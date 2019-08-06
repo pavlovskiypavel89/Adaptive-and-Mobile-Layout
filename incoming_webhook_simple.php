e39622024cb25ed970b8874f0c39378302b06e6a
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2019-08-06 11:10:52 +0700

    Update incoming_webhook_simple.php

--- incoming_webhook_simple.php
+++ incoming_webhook_simple.php
@@ -1,23 +1,22 @@
 <?
 
-    $queryUrl = 'https://restapi.bitrix24.ru/rest/1/ng5iy6mlibyijomy/user.current.json';
-    // $queryData = http_build_query(array());
+print_r($_REQUEST);
+writeToLog($_REQUEST, 'incoming');
 
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_SSL_VERIFYPEER => 0,
-        CURLOPT_POST => 1,
-        CURLOPT_HEADER => 0,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $queryUrl,
-        //CURLOPT_POSTFIELDS => $queryData,
-    ));
-
-    $result = curl_exec($curl);
-    curl_close($curl);
-
-
-    echo "<pre>";
-    print_r($result);
-    print_r(json_decode($result, true));
-    echo "</pre>";
+/**
+ * Write data to log file.
+ *
+ * @param mixed $data
+ * @param string $title
+ *
+ * @return bool
+ */
+function writeToLog($data, $title = '') {
+ $log = "\n------------------------\n";
+ $log .= date("Y.m.d G:i:s") . "\n";
+ $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
+ $log .= print_r($data, 1);
+ $log .= "\n------------------------\n";
+ file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
+ return true;
+} 
