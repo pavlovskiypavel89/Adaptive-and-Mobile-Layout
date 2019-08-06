cc09386aa260b15c41febb00a18fae61da522f7d
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2019-08-06 11:08:07 +0700

    Add files via upload

---
+++ incoming_webhook_simple.php
@@ -0,0 +1,23 @@
+<?
+
+    $queryUrl = 'https://restapi.bitrix24.ru/rest/1/ng5iy6mlibyijomy/user.current.json';
+    // $queryData = http_build_query(array());
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $queryUrl,
+        //CURLOPT_POSTFIELDS => $queryData,
+    ));
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+
+
+    echo "<pre>";
+    print_r($result);
+    print_r(json_decode($result, true));
+    echo "</pre>";
